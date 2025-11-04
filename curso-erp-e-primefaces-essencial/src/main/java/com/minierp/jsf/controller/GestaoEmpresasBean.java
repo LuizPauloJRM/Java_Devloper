f109b30556f21253601fdc6045c72f6b3eaededb
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-11-04 18:35:14 -0300

    desabilitando editar e excluir se nao for selecionado

--- curso-erp-e-primefaces-essencial/src/main/java/com/minierp/jsf/controller/GestaoEmpresasBean.java
+++ curso-erp-e-primefaces-essencial/src/main/java/com/minierp/jsf/controller/GestaoEmpresasBean.java
@@ -116,5 +116,8 @@ public class GestaoEmpresasBean implements Serializable {
     public Empresa getEmpresa() {
 		return empresa;
 	}
+    public boolean isEmpresaSelecionada() {
+    	return empresa != null && empresa.getId() !=null ;
+    }
     
 }
